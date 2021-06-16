f8d85fc173daf2c7590c85d1e67681a9dd4cf648
wangchao <wangchao>
2021-06-16 12:23:26 +0800

    0616fs使用总结

--- 02file/09fs中的其他方法.js
+++ 02file/09fs中的其他方法.js
@@ -65,5 +65,61 @@ fs.unlinkSync(path)
 /*
 fs.truncate(path[, len], callback)
 fs.truncateSync(path[, len])
-    --截断文件
+    --截断文件,将文件修改为指定大小
 */
+// fs.truncateSync('hello4.txt',10)
+
+
+/*
+fs.mkdir(path[, options], callback)
+fs.mkdirSync(path[, options])
+    --创建一个目录
+*/
+// fs.mkdirSync('Hellods')
+
+/*
+fs.rmdir(path[, options], callback)
+fs.rmdirSync(path[, options])
+    --删除一个目录
+*/
+
+// fs.rmdirSync('Hellods')
+
+
+
+/*
+fs.rename(oldPath, newPath, callback)
+fs.renameSync(oldPath, newPath)
+    --对文件进行重命名
+        --参数
+            oldPath 旧的路径
+            newPath 新的路径
+            callback 回调函数
+*/
+
+// fs.rename('hello2.txt','hello.txt',(err)=>{
+//     if(!err){
+//         console.log("修改成功了！");
+//     }else{
+//         console.error(err);
+//     }
+// })
+
+
+/*
+fs.watchFile(filename[, options], listener)
+    --监视文件的修改
+    --参数：
+        filename要监视的文件的名字
+        options配置选项
+        listener 回调函数，当文件发生变化是，回调函数执行
+            listener(curr,prev)
+                curr 当前文件的状态
+                prev 修改前文件的状态
+                --这两个对象都是stats对象
+*/
+
+fs.watchFile('hello.txt',(curr,prev)=>{
+    console.log('当前文件大小:',prev.size);
+    console.log("修改后文件大小：",curr.size);
+})
